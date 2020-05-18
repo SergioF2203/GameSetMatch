92a9d1ba68d9e168050a7f318a8f573e0ff5bdb5
SergioF <fs220381@gmail.com>
2020-05-18 13:31:15 +0300

    + UML diagram

---
+++ GameSetMatch/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameSetMatch
+{
+    public class Game
+    {
+        private array<Player> players;
+        private int setCount;
+        private int duringMatch;
+    }
+}
